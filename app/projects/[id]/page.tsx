a429ac2b7b64d6c0c82fa7d75fc7622f7b74fa17
yejinyao <wb.yejinyao01@mesg.corp.netease.com>
2026-06-25 19:29:24 +0800

    fix: remove stale projectId prop from UrlDeleteButton call site

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -130,7 +130,6 @@ export default async function ProjectDetailPage({
                     <UrlDeleteButton
                       urlId={url.id}
                       urlAlias={url.alias || url.url}
-                      projectId={id}
                     />
                   </div>
                 </div>
